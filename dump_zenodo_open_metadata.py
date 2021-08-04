cccc9f03bca1d4728fecd050f45998f4df309055
ctrl-alt-soneca <79139616+ctrl-alt-soneca@users.noreply.github.com>
2021-08-04 14:38:13 +0200

    scripts: updated dump_zenodo_open_metadata

--- dump_zenodo_open_metadata.py
+++ dump_zenodo_open_metadata.py
@@ -53,53 +53,68 @@ def parse_record(rec, spam=False):
     out["spam"] = spam
     return out
 
+DUMP_TYPES = {
+  '1': 'ham',
+  '2': 'spam',
+  '3': 'full',
+}
+
+choices = [k + ' - ' + v for k, v in DUMP_TYPES.items()]
+choice = str(input("What type of dump you want? {}".format(', '.join(choices)) + '\n'))
+dump_type = DUMP_TYPES.get(choice)
+
+if not dump_type:
+    raise Exception('Invalid dump type {}'.format(choice))
+
 today = datetime.date.today().isoformat()
-FILENAME = f"zenodo_open_metadata_{today}.jsonl.gz"
+FILENAME = "zenodo_open_metadata_" + dump_type + '_' + today + ".jsonl.gz"
 
-records_ham = (
-    db.session.query(RecordMetadata)
-    .join(PersistentIdentifier, PersistentIdentifier.object_uuid == RecordMetadata.id)
-    .filter(
-        PersistentIdentifier.pid_type == "recid",
-        PersistentIdentifier.status == "R",
-        PersistentIdentifier.object_type == "rec",
+if dump_type in ('spam', 'full'):
+    records_ham = (
+        db.session.query(RecordMetadata)
+        .join(PersistentIdentifier, PersistentIdentifier.object_uuid == RecordMetadata.id)
+        .filter(
+            PersistentIdentifier.pid_type == "recid",
+            PersistentIdentifier.status == "R",
+            PersistentIdentifier.object_type == "rec",
+        )
     )
-)
 
-# Write the non-spam records first
-total_ham = records_ham.count()
-with gzip.open(FILENAME, "wb") as fp:
-    for idx, r in enumerate(records_ham.yield_per(1000)):
-        if idx % 10000 == 0:
-            print('[{}]: {} / {}'.format(datetime.datetime.now().isoformat(), idx, total_ham))
-        if r.json is not None:
-            rec = parse_record(Record(r.json, model=r), spam=False)
-            fp.write(json.dumps(rec) + "\n")
-
-records_spam = (
-    db.session.query(RecordMetadata)
-    .join(PersistentIdentifier, PersistentIdentifier.object_uuid == RecordMetadata.id)
-    .filter(
-        PersistentIdentifier.pid_type == "recid",
-        PersistentIdentifier.status == "D",
-        PersistentIdentifier.object_type == "rec",
-    )
-)
-
-# Write the SPAM records
-failing_records = []
-total_spam = records_spam.count()
-with gzip.open(FILENAME, "ab") as fp:
-    for idx, r in enumerate(records_spam.yield_per(1000)):
-        if idx % 1000 == 0:
-            print('[{}]: {} / {}'.format(datetime.datetime.now().isoformat(), idx, total_spam))
-        try:
-            if (
-                r.json is not None
-                and "removal_reason" in r.json
-                and "spam" in r.json["removal_reason"].lower()
-            ):
-                rec = parse_record(Record(r.json, model=r).revisions[-2], spam=True)
+    # Write the non-spam records first
+    total_ham = records_ham.count()
+    with gzip.open(FILENAME, "ab") as fp:
+        for idx, r in enumerate(records_ham.yield_per(1000)):
+            if idx % 10000 == 0:
+                print('[{}]: {} / {}'.format(datetime.datetime.now().isoformat(), idx, total_ham))
+            if r.json is not None:
+                rec = parse_record(Record(r.json, model=r), spam=False)
                 fp.write(json.dumps(rec) + "\n")
-        except Exception as ex:
-            failing_records.append((r, ex))
+
+if dump_type in ('ham', 'full'):
+    records_spam = (
+        db.session.query(RecordMetadata)
+        .join(PersistentIdentifier, PersistentIdentifier.object_uuid == RecordMetadata.id)
+        .filter(
+            PersistentIdentifier.pid_type == "recid",
+            PersistentIdentifier.status == "D",
+            PersistentIdentifier.object_type == "rec",
+        )
+    )
+
+    # Write the SPAM records
+    failing_records = []
+    total_spam = records_spam.count()
+    with gzip.open(FILENAME, "ab") as fp:
+        for idx, r in enumerate(records_spam.yield_per(1000)):
+            if idx % 1000 == 0:
+                print('[{}]: {} / {}'.format(datetime.datetime.now().isoformat(), idx, total_spam))
+            try:
+                if (
+                    r.json is not None
+                    and "removal_reason" in r.json
+                    and "spam" in r.json["removal_reason"].lower()
+                ):
+                    rec = parse_record(Record(r.json, model=r).revisions[-2], spam=True)
+                    fp.write(json.dumps(rec) + "\n")
+            except Exception as ex:
+                failing_records.append((r, ex))
